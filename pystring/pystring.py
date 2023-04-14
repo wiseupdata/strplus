3b57e57ef52b579145e6dc9953a71a0adb5c29a8
silllvio <silllvio@hotmail.com>
2023-04-14 07:33:45 +0100

    sync

--- pystring/pystring.py
+++ pystring/pystring.py
@@ -1,21 +1,17 @@
 class Str(str):
-    def __init__(self, wrapped_str):
+    def __new__(cls, wrapped_str):
         if not isinstance(wrapped_str, str):
             raise TypeError("wrapped_str must be a string")
-        super().__init__()
-        self.update(wrapped_str)
+        return super().__new__(cls, wrapped_str)
 
     def __getattr__(self, attr):
-        return getattr(self, attr)
+        return super().__getattribute__(attr)
 
     def __getitem__(self, key):
-        item = super().__getitem__(key)
-        if isinstance(item, str):
-            return Str(item)
-        return item
-
-    def __instancecheck__(self, instance):
-        return isinstance(instance, str)
-
-    def __subclasscheck__(self, subclass):
-        return issubclass(subclass, str)
+        if isinstance(key, int):
+            item = super().__getitem__(key)
+            if isinstance(item, str):
+                return Str(item)
+            return item
+        else:
+            raise TypeError("indices must be integers")
