ef990d399f071176da568d30a7b9678aac911631
silllvio <silllvio@hotmail.com>
2023-04-14 17:41:38 +0100

    sync

--- strplus/cases.py
+++ strplus/cases.py
@@ -27,9 +27,11 @@ def to_pascal(text):
 
 import re
 
+import re
+
 def to_camel(text):
     """
-    Convert a string from any case to CamelCase.
+    Convert a string from any case to camelCase.
     """
     text = text.strip()
     
@@ -42,13 +44,14 @@ def to_camel(text):
     # Capitalize the first letter of each word except the first word
     capitalized_words = []
     for i, word in enumerate(words):
-        if i == 0 and word[0].isdigit():
-            capitalized_words.append(str(word[0]))
-            word = word[1:]
-        capitalized_words.append(word.capitalize())
+        if i == 0:
+            capitalized_words.append(word.lower())
+        else:
+            capitalized_words.append(word.capitalize())
     
-    # Join the words back together to form the CamelCase string
+    # Join the words back together to form the camelCase string
     return ''.join(capitalized_words)
+
 
 
 
