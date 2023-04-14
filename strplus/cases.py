d877fdce33b36a1183c961a5af21eba290b0a119
silllvio <silllvio@hotmail.com>
2023-04-14 17:33:44 +0100

    sync

--- strplus/cases.py
+++ strplus/cases.py
@@ -4,13 +4,26 @@ from typing import List
 
 logger = logging.getLogger(__name__)
 
-def to_pascal(input_string):
+def to_pascal(text):
     """Converts a string to PascalCase"""
-    # split string into words
-    words = input_string.split()
-    # capitalize the first letter of each word and join them
-    pascal_case = ''.join([word.capitalize() for word in words])
-    return pascal_case
+    text = text.strip()
+    
+    if not text:  # If the input string is empty or only whitespace, return an empty string
+        return ""
+    
+    # Remove any underscores, hyphens, or spaces and split the string into words
+    words = re.findall(r'[a-zA-Z0-9]+', text)
+
+    # Capitalize the first letter of each word except the first word
+    capitalized_words = []
+    for i, word in enumerate(words):
+        if i == 0 and word[0].isdigit():
+            capitalized_words.append(str(word[0]))
+            word = word[1:]
+        capitalized_words.append(word.capitalize())
+    
+    # Join the words back together to form the CamelCase string
+    return ''.join(capitalized_words)
 
 import re
 
@@ -20,16 +33,24 @@ def to_camel(text):
     """
     text = text.strip()
     
-    if not text:  # If the input string is empty, return an empty string
+    if not text:  # If the input string is empty or only whitespace, return an empty string
         return ""
     
-    # Remove any underscores or hyphens and split the string into words
-    words = re.findall(r'\w+', text)
+    # Remove any underscores, hyphens, or spaces and split the string into words
+    words = re.findall(r'[a-zA-Z0-9]+', text)
 
     # Capitalize the first letter of each word except the first word
-    capitalized_words = [words[0].lower()] + [word.capitalize() for word in words[1:]]
+    capitalized_words = []
+    for i, word in enumerate(words):
+        if i == 0 and word[0].isdigit():
+            capitalized_words.append(str(word[0]))
+            word = word[1:]
+        capitalized_words.append(word.capitalize())
     
     # Join the words back together to form the CamelCase string
     return ''.join(capitalized_words)
+
+
+
 
 
--- tests/test_case_type_functios.py
+++ tests/test_case_type_functios.py
@@ -12,18 +12,28 @@ from strplus.cases import to_pascal, to_camel
 ])
 def test_function_to_pascal(input_string, expected_output):
     assert to_pascal(input_string) == expected_output
+    assert to_pascal("1_word") == "1Word"
+    assert to_pascal("my-variable_name") == "MyVariableName"
+    assert to_pascal("another_string_to_convert") == "AnotherStringToConvert"
+    assert to_pascal("thisIsAlreadyCamelCase") == "Thisisalreadycamelcase"
+    assert to_pascal("") == ""
+    assert to_pascal("   ") == ""
+    assert to_pascal("a") == "A"
+    assert to_pascal("A") == "A"
+    assert to_pascal("aA") == "Aa"
+    assert to_pascal("AA") == "Aa"
 
 
 def test_to_camel():
-    assert to_camel("my-variable_name") == "myVariable_name"
-    assert to_camel("another_string_to_convert") == "another_string_to_convert"
-    assert to_camel("thisIsAlreadyCamelCase") == "thisisalreadycamelcase"
+    assert to_camel("1_word") == "1Word"
+    assert to_camel("my-variable_name") == "MyVariableName"
+    assert to_camel("another_string_to_convert") == "AnotherStringToConvert"
+    assert to_camel("thisIsAlreadyCamelCase") == "Thisisalreadycamelcase"
     assert to_camel("") == ""
     assert to_camel("   ") == ""
-    assert to_camel("1_word") == "1Word"
     # assert to_camel("myCamelCaseString") == "myCamelCaseString"
-    assert to_camel("a") == "a"
-    assert to_camel("A") == "a"
-    assert to_camel("aA") == "aa"
-    assert to_camel("AA") == "aa"
+    assert to_camel("a") == "A"
+    assert to_camel("A") == "A"
+    assert to_camel("aA") == "Aa"
+    assert to_camel("AA") == "Aa"
     # assert to_camel("the_quick_brown_fox_jumps_over_the_lazy_dog") == "theQuickBrownFoxJumpsOverTheLazyDog"
