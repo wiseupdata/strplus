ff01db2ce992af35b35941423cd4e12b98dec275
silllvio <silllvio@hotmail.com>
2023-04-15 11:04:02 +0100

    sync

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
 project = 'strplus'
 copyright = '2023, WiseUpData'
 author = 'WiseUpData'
@@ -13,8 +17,11 @@ release = '0.0.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +31,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'yummy_sphinx_theme'
 html_static_path = ['_static']
