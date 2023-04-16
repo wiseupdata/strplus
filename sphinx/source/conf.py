69833d603b6c3de25eb6a78eebcbbd8b41d44c7a
silllvio <silllvio@hotmail.com>
2023-04-16 21:31:46 +0100

    sync

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -9,10 +9,27 @@ copyright = "2023, WiseUpData"
 author = "WiseUpData"
 release = "0.0.8"
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.todo"]
+extensions = [
+    
+"sphinx.ext.autodoc",
+   "sphinx.ext.doctest",
+   "sphinx.ext.extlinks",
+   "sphinx.ext.intersphinx",
+   "sphinx.ext.todo",
+   "sphinx.ext.mathjax",
+   "sphinx.ext.viewcode",
+   "sphinx_copybutton"    
+]
+
+
+autosummary_generate = True
+autoclass_content = "class"
+
+
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_show_sourcelink = True
 
 html_theme = "sphinx_material"
 
@@ -20,7 +37,7 @@ html_theme_options = {
     "base_url": "https://wiseupdata.github.io/strplus/",
     "repo_url": "https://github.com/wiseupdata/strplus/",
     "repo_name": "STRPLUS",
-    "globaltoc_depth": 3,
+    "globaltoc_depth": -1,
     "theme_color": "e92164",
     "color_primary": "pink",
     "css_minify": True,
@@ -28,13 +45,17 @@ html_theme_options = {
     "color_accent": "teal",
     # "logo_icon": "&#xe021",
     "logo_icon": "&#xE88A",
-    'nav_title': 'Extend your string powers with Str+',
+    'nav_title': 'strplus package',
     "nav_links": [
         {"href": "https://wiseupdata.github.io/strplus/", "title": "Home", "internal": False},
         {"href": "https://github.com/wiseupdata/strplus", "title": "GitHub", "internal": False},
         {"href": "https://github.com/wiseupdata", "title": "GitHub Site", "internal": False},
         {"href": "https://wiseupdata.com", "title": "Site", "internal": False},
     ],
+     "heroes": {
+        "index": "Extend your string powers with Str+",
+        "customization": "Configuration options to personalize your site."
+    }
 }
 
 html_static_path = ["_static"]
